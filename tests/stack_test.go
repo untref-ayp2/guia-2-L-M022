69bad1b13a91acc0d40d0a291fab324c83cff15f
Lm <68245933+L-M022@users.noreply.github.com>
2023-03-29 16:48:34 -0300

    ejercicios

--- tests/stack_test.go
+++ tests/stack_test.go
@@ -12,9 +12,22 @@ func TestPush(t *testing.T) {
 	s.Push(2)
 	s.Push(3)
 
-	if s[0] != 1 || s[1] != 2 || s[2] != 3 {
+	aux, _ := s.Pop()
+	if aux != 3 {
 		t.Error("Error en Push")
 	}
+	aux, _ = s.Pop()
+	if aux != 2 {
+		t.Error("Error en Push")
+	}
+	aux, _ = s.Pop()
+	if aux != 1 {
+		t.Error("Error en Push")
+	}
+
+	// if s[0] != 1 || s[1] != 2 || s[2] != 3 {
+	// 	t.Error("Error en Push")
+	// }
 }
 
 func TestPop(t *testing.T) {
